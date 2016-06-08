905339bb1b8b8986aaa4e6c8cb71251ac0a592a4
Kathy Chang <emailkathy@gmail.com>
2016-06-08 12:52:42 -0400

    Finished all the model tests

--- tests/server/models/user-test.js
+++ tests/server/models/user-test.js
@@ -1,6 +1,9 @@
+// KC: Unclear what sinon is about. Maybe encryption?
 var sinon = require('sinon');
 var expect = require('chai').expect;
 
+// KC: Sequelize, dbURI, db are replacing what next line usually does:
+// var db = require('../path/to/database')
 var Sequelize = require('sequelize');
 var dbURI = 'postgres://localhost:5432/testing-fsg';
 var db = new Sequelize(dbURI, {
@@ -16,6 +19,112 @@ describe('User model', function () {
     beforeEach('Sync DB', function () {
        return db.sync({ force: true });
     });
+
+    // ----------  Start of tests for data types, validation.  ----------
+
+    // KC: Other tests to add if User model is updated:
+    // - password is required (allowNull: false)
+
+    describe('validations', function() {
+
+        it('has email and name fields of type String', function() {
+            return User.create({
+                email: 'omg@wtf.com',
+                name: 'Kloo Less',
+                isAdmin: true
+            })
+            .then(function(savedUser) {
+                expect(savedUser.email).to.equal('omg@wtf.com');
+                expect(savedUser.email).to.be.a('string');
+                expect(savedUser.name).to.equal('Kloo Less');
+                expect(savedUser.name).to.be.a('string');
+            }); 
+        });
+
+        it('requires email', function() {
+            return User.build({
+                name: 'Kloo Less',
+                isAdmin: true
+            })
+            .validate()
+            .then(function(result) {
+                expect(result).to.be.an('object');
+                expect(result.message).to.equal('notNull Violation: email cannot be null');
+            });
+        });
+
+        it('requires a valid email', function() {
+            return User.build({
+                email: 'omg@wtf,com',
+                name: 'Kloo Less',
+                isAdmin: true
+            })
+            .validate()
+            .then(function(result) {
+                expect(result).to.be.an('object');
+                expect(result.message).to.equal('Validation error: Validation isEmail failed');
+            });
+        });
+
+        it('requires a unique email', function() {
+            var user1 = User.create({
+                email: 'omg@wtf.com',
+                name: 'Kloo Less',
+                isAdmin: true
+            });
+
+            // Trying to create another user with the same email as user1.
+            return User.create({
+                email: 'omg@wtf.com',
+                name: 'Ida Noh',
+                isAdmin: false
+            })
+            .then(function(savedUser) {
+                // This will throw an error, which catch will handle.
+                expect(savedUser.email).to.equal('omg@wtf.com');
+            })
+            .catch(function(err) {
+                expect(err).to.exist;
+                // console.log err to see where err.errors[0].type comes from:
+                // console.log("BOOOOOOOOO: ", err.errors[0].type);
+                expect(err.errors[0].type).to.equal('unique violation');
+            });
+        });
+
+        it('has isAdmin field of type Boolean', function() {
+            return User.create({
+                email: 'omg@wtf.com',
+                name: 'Kloo Less',
+                isAdmin: true
+            })
+            .then(function(savedUser) {
+                expect(savedUser.isAdmin).to.equal(true);
+                expect(savedUser.isAdmin).to.be.a('boolean');
+            });
+        });
+
+        it('requires isAdmin', function() {
+            return User.build({
+                email: 'omg@wtf.com',
+                name: 'Kloo Less',
+            })
+            .validate()
+            .then(function(result) {
+                expect(result).to.be.an('object');
+                expect(result.message).to.equal('notNull Violation: isAdmin cannot be null');
+            });
+        });
+
+    });  // ends describe validations block
+
+
+
+
+    // ----------  End of tests for data types, validation.  ----------
+
+
+
+    // ----------  Start of tests for encryption (included with FSG). ----------
 
     describe('password encryption', function () {
 
@@ -95,7 +204,10 @@ describe('User model', function () {
             var saltSpy;
 
             var createUser = function () {
-                return User.create({ email: 'obama@gmail.com', password: 'potus' });
+                return User.create({
+                    email: 'omg@wtf.com', 
+                    password: 'WritingTestsIsHard',
+                    isAdmin: true });
             };
 
             beforeEach(function () {
@@ -111,7 +223,7 @@ describe('User model', function () {
             it('should call User.encryptPassword with the given password and generated salt', function (done) {
                 createUser().then(function () {
                     var generatedSalt = saltSpy.getCall(0).returnValue;
-                    expect(encryptSpy.calledWith('potus', generatedSalt)).to.be.ok;
+                    expect(encryptSpy.calledWith('WritingTestsIsHard', generatedSalt)).to.be.ok;
                     done();
                 });
             });
@@ -137,7 +249,10 @@ describe('User model', function () {
         describe('sanitize method', function () {
 
             var createUser = function () {
-                return User.create({ email: 'obama@gmail.com', password: 'potus' });
+                return User.create({
+                    email: 'omg@wtf.com', 
+                    password: 'WritingTestsIsHard',
+                    isAdmin: true });
             };
 
             it('should remove sensitive information from a user object', function () {
@@ -152,5 +267,7 @@ describe('User model', function () {
         });
 
     });
+
+    // ----------  End of tests for encryption (included with FSG). ----------
 
 });
