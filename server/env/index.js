c3bfe6ea1cf386e35c247bf41b7dc0c0b1e54631
Nichole Bates <nbates88@gmail.com>
2016-06-07 21:49:42 -0400

    some updates

--- server/env/index.js
+++ server/env/index.js
@@ -1,9 +1,12 @@
 var path = require('path');
 var devConfigPath = path.join(__dirname, './development.js');
+var testConfigPath = path.join(__dirname, './test.js');
 var productionConfigPath = path.join(__dirname, './production.js');
 
 if (process.env.NODE_ENV === 'production') {
     module.exports = require(productionConfigPath);
-} else {
+} else if(process.env.NODE_ENV === 'testing'){
+    module.exports = require(testConfigPath);
+}else {
     module.exports = require(devConfigPath);
 }
