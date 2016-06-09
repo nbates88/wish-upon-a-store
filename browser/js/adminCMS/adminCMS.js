6c2a5a37fc117aefbb3f8c8cd4c7b378612ff7fb
borisonr <borisonr@gmail.com>
2016-06-09 13:39:39 -0400

    created child states for the admin panel

--- browser/js/adminCMS/adminCMS.js
+++ browser/js/adminCMS/adminCMS.js
@@ -7,7 +7,28 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/adminCMS/adminCMS.html'
     });
 
+    $stateProvider.state('admin.users', {
+        url: '/users',
+        templateUrl: 'js/adminCMS/adminCMSusers.html'
+    });
+
+    $stateProvider.state('admin.products', {
+        url: '/products',
+        templateUrl: 'js/adminCMS/adminCMSproducts.html'
+    });
+
+    $stateProvider.state('admin.collections', {
+        url: '/collections',
+        templateUrl: 'js/adminCMS/adminCMScollections.html'
+    });
+
+    $stateProvider.state('admin.orders', {
+        url: '/orders',
+        templateUrl: 'js/adminCMS/adminCMSorders.html'
+    });
+
 });
+
 
 app.controller('AdminCtrl', function($scope){
     
