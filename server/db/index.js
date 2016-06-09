fd14fd6188ebbf805ed55bf7f48b86ea7c466b32
Anna Garcia <annaegarcia@gmail.com>
2016-06-09 15:41:24 -0400

    add product to an order

--- server/db/index.js
+++ server/db/index.js
@@ -10,13 +10,12 @@ var Collection = require('./models/collection')(db);
 var Wishlist = require('./models/wishlist')(db);
 
 Product.belongsToMany(Order, {through: 'OrderProducts'});
+Order.belongsToMany(Product, {through: 'OrderProducts'});
 // Order.belongsTo(User, {foreignKey: 'userId'});
 User.hasMany(Order, {as: 'Orders'});
 Review.belongsTo(Product);
 Review.belongsTo(User);
 Product.belongsToMany(Collection, {through: 'CollectionProduct'});
-//
 Collection.belongsToMany(Product, {through: 'CollectionProduct'});
-//
 Product.belongsToMany(Wishlist, {through: 'WishlistProduct'});
 Wishlist.belongsTo(User);
