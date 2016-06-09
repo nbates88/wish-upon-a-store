ce9e9685849ba3529ad38a2f04b48d8841b96015
borisonr <borisonr@gmail.com>
2016-06-09 17:20:30 -0400

    cleaned up myaccountfactory

--- browser/js/common/factories/MyAccountFactory.js
+++ browser/js/common/factories/MyAccountFactory.js
@@ -2,9 +2,7 @@ app.factory('MyAccountFactory', function($http) {
 
     var getMyAccount = function() {
         return $http.get('/api/members/myaccount').then(function(response) {
-            console.log('data retrieved:', response.data)
             return response.data;
-            myAccount = response.data;
         });
     };
 
