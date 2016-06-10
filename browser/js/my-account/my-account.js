680aacf312e1b2f00092c267d77ccc6bf2b26284
Yini Shi <yini.shi@gmail.com>
2016-06-10 15:43:10 -0400

    moved format date function to utils

--- browser/js/my-account/my-account.js
+++ browser/js/my-account/my-account.js
@@ -1,22 +1,3 @@
-let formatDate = function(rawDateString) {
-    let date = rawDateString.slice(0, 10).split('-');
-    let months = {
-        '01': 'January',
-        '02': 'February',
-        '03': 'March',
-        '04': 'April',
-        '05': 'May',
-        '06': 'June',
-        '07': 'July',
-        '08': 'August',
-        '09': 'September',
-        '10': 'October',
-        '11': 'November',
-        '12': 'December',
-    };
-    return `${months[date[1]]} ${date[2]}, ${date[0]}`;
-}
-
 app.config(function($stateProvider) {
     $stateProvider.state('myAccount', {
         url: '/my-account',
