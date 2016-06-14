28aad904b9fddf6d5e768371efe12519756297bd
Yini Shi <yini.shi@gmail.com>
2016-06-14 16:14:32 -0400

    fix my orders

--- browser/js/common/factories/MyAccountFactory.js
+++ browser/js/common/factories/MyAccountFactory.js
@@ -2,14 +2,20 @@ app.factory('MyAccountFactory', function($http) {
 
     return {
         getMyAccount: function() {
-            console.log('getting my')
-            return $http.get('/api/members/myaccount').then(function(response) {
-                console.log("getMyAccount response.data: ", response.data);
-                return response.data;
-            });
+            return $http.get('/api/members/myaccount')
+                .then(response => response.data);
         },
         getMyOrders: function() {
-        	return $http.get('/api/orders/')
+            return $http.get('/api/orders/user')
+                .then(response => {
+                    response.data.forEach(order => {
+                        order._createdAt = formatDate(order._createdAt);
+                        order._updatedAt = formatDate(order._updatedAt);
+
+                    })
+                    return response.data;
+
+                })
         }
     };
 
--- browser/js/my-account/controllers/MyOrdersCtrl.js
+++ browser/js/my-account/controllers/MyOrdersCtrl.js
@@ -2,14 +2,4 @@ app.controller('MyOrdersCtrl', function($scope, orders) {
 
 	$scope.orders = orders
 
-    // MyAccountFactory.getMyAccount()
-    //     .then(function(response) {
-    //         response.Orders.forEach(order => {
-    //             order._createdAt = formatDate(order._createdAt);
-    //             order._updatedAt = formatDate(order._updatedAt);
-    //         });
-    //         $scope.orders = response.Orders
-    //     });
-
-
 })
