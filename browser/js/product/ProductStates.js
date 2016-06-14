7e6ca23269b5f39c11d4a9b14461801a4b2fd6df
Yini Shi <yini.shi@gmail.com>
2016-06-14 10:54:44 -0400

    small refactor of product states

--- browser/js/product/ProductStates.js
+++ browser/js/product/ProductStates.js
@@ -1,61 +1,52 @@
 app.config(function($stateProvider) {
+
     $stateProvider.state('product', {
-        url: '/products/:id',
-        controller: 'ProductCtrl',
-        templateUrl: 'js/product/templates/product.html',
-        resolve: {
-            product: function($stateParams, ProductFactory) {
-                var id = $stateParams.id;
-                return ProductFactory.getOneProduct(id)
-            },
-            user: function(AuthService) {
-                return AuthService.getLoggedInUser();
+            url: '/products/:id',
+            controller: 'ProductCtrl',
+            templateUrl: 'js/product/templates/product.html',
+            // abstract: true,
+            resolve: {
+                product: function($stateParams, ProductFactory) {
+                    var id = $stateParams.id;
+                    return ProductFactory.getOneProduct(id)
+                },
+                user: function(AuthService) {
+                    return AuthService.getLoggedInUser();
 
+                }
             }
-        }
-    });
-
-});
-
-app.config(function($stateProvider) {
-    $stateProvider.state('product.allReviews', {
-        url: '/all-reviews',
-        controller: 'AllReviewsCtrl',
-        parent: 'product',
-        templateUrl: 'js/product/templates/all-reviews.html',
-        resolve: {
-            reviews: function($stateParams, ReviewFactory) {
-                var id = $stateParams.id;
-                return ReviewFactory.getProductReviews(id)
+        })
+        .state('product.allReviews', {
+            url: '/all-reviews',
+            controller: 'AllReviewsCtrl',
+            // parent: 'product',
+            templateUrl: 'js/product/templates/all-reviews.html',
+            resolve: {
+                reviews: function($stateParams, ReviewFactory) {
+                    var id = $stateParams.id;
+                    return ReviewFactory.getProductReviews(id)
+                }
             }
-        }
-    });
+        })
+        .state('product.addReview', {
+            url: '/add-review',
+            controller: 'AddReviewCtrl',
+            parent: 'product',
+            templateUrl: 'js/product/templates/add-review.html',
+            resolve: {
+                product: function($stateParams, ProductFactory) {
+                    var id = $stateParams.id;
+                    return ProductFactory.getOneProduct(id)
+                },
+                user: function(AuthService) {
+                    return AuthService.getLoggedInUser();
+
+                },
+            }
+        })
+        .state('reviewSuccess', {
+            url: '/review-submitted',
+            templateUrl: 'js/product/templates/success.html'
+        })
 
 });
-
-app.config(function($stateProvider) {
-    $stateProvider.state('product.addReview', {
-        url: '/add-review',
-        controller: 'AddReviewCtrl',
-        parent: 'product',
-        templateUrl: 'js/product/templates/add-review.html',
-        resolve: {
-            product: function($stateParams, ProductFactory) {
-                var id = $stateParams.id;
-                return ProductFactory.getOneProduct(id)
-            },
-            user: function(AuthService) {
-                return AuthService.getLoggedInUser();
-
-            },
-        }
-    });
-
-});
-
-app.config(function($stateProvider) {
-    $stateProvider.state('reviewSuccess', {
-        url: '/review-submitted',
-        templateUrl: 'js/product/templates/success.html'
-    })
-})
