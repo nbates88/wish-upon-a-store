842361b1b90b2852f8d5c3c6c3e74591432ea71e
Yini Shi <yini.shi@gmail.com>
2016-06-13 21:31:29 -0400

    refactor go back function

--- browser/js/product/ProductStates.js
+++ browser/js/product/ProductStates.js
@@ -56,11 +56,6 @@ app.config(function($stateProvider) {
 app.config(function($stateProvider) {
     $stateProvider.state('reviewSuccess', {
         url: '/review-submitted',
-        templateUrl: 'js/product/templates/success.html',
-        controller: function($scope) {
-            $scope.goBack = function() {
-                window.history.back();
-            };
-        }
+        templateUrl: 'js/product/templates/success.html'
     })
 })
